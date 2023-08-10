89a7580a67b71564b2354e9f0589a13bad13717f
yashPundhir <yash.pundhir.prof@gmail.com>
2023-08-10 23:47:59 +0530

    Hero Section done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,35 @@
+"use client";
+
 import Image from "next/image";
+import { CustomButton } from "./";
 
 const Hero = () => {
-	return <div>Hero</div>;
+	const handleScroll = () => {};
+
+	return (
+		<div className="hero">
+			<div className="flex-1 pt-36 padding-x">
+				<h1 className="hero__title">
+					Find, Book, or Rent a Car — Quickly and Easily!
+				</h1>
+				<p className="hero__subtitle">
+					Streamline your car rental experience with our effortless booking
+					process.
+				</p>
+				<CustomButton
+					title="Explore Cars"
+					containerStyles="bg-primary-blue text-white rounded-full mt-10 font-medium text-lg"
+					handleClick={handleScroll}
+				/>
+			</div>
+			<div className="hero__image-container">
+				<div className="hero__image">
+					<Image src="/hero2.png" alt="hero" fill className="object-contain" />
+					<div className="overflow-hidden hero__image-overlay" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
