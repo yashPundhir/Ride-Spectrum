162efafdb3891cb3ae38c5fa7cc43bd1446735fd
yashPundhir <yash.pundhir.prof@gmail.com>
2023-08-11 23:48:04 +0530

    components export file updated

--- components/index.js
+++ components/index.js
@@ -2,5 +2,16 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Footer, Navbar };
+export {
+	Hero,
+	CustomButton,
+	SearchManufacturer,
+	Footer,
+	Navbar,
+	CustomFilter,
+	SearchBar,
+};
