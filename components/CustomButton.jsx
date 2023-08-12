7a6962f44a2bf59624b55d489ee8aa2aaa6520fe
yashPundhir <yash.pundhir.prof@gmail.com>
2023-08-12 23:45:38 +0530

    CustomButton UI updated wrt rightIcon prop

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -2,7 +2,14 @@
 
 import Image from "next/image";
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType }) => {
+const CustomButton = ({
+	title,
+	containerStyles,
+	handleClick,
+	btnType,
+	textStyles,
+	rightIcon,
+}) => {
 	return (
 		<button
 			disabled={false}
@@ -10,7 +17,17 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType }) => {
 			className={`custom-btn ${containerStyles}`}
 			onClick={handleClick}
 		>
-			<span className={`flex-1`}>{title}</span>
+			<span className={`flex-1 ${textStyles}`}>{title}</span>
+			{rightIcon && (
+				<div className=" relative w-6 h-6">
+					<Image
+						src={rightIcon}
+						alt="right-icon"
+						fill
+						className="object-contain"
+					/>
+				</div>
+			)}
 		</button>
 	);
 };
