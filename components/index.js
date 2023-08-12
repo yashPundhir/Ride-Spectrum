916bdf1154c097ae4c76b4496a81996488d62b6c
yashPundhir <yash.pundhir.prof@gmail.com>
2023-08-12 23:52:16 +0530

    components export file updated

--- components/index.js
+++ components/index.js
@@ -5,6 +5,8 @@ import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
 export {
 	Hero,
@@ -14,4 +16,6 @@ export {
 	Navbar,
 	CustomFilter,
 	SearchBar,
+	CarCard,
+	CarDetails,
 };
