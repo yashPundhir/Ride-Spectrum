d9836a2a373617095922a465d8597727209a1073
yashPundhir <yash.pundhir.prof@gmail.com>
2023-08-13 23:52:02 +0530

    respective car images added using carImage API

--- components/CarDetails.jsx
+++ components/CarDetails.jsx
@@ -4,62 +4,10 @@ import Image from "next/image";
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 
+import { generateCarImageUrl } from "@/utils";
+
 const CarDetails = ({ isOpen, closeModal, car }) => {
 	return (
-		// <Transition appear show={isOpen} as={Fragment}>
-		// 	<Dialog as="div" className="relative z-10" onClose={closeModal}>
-		// 		<Transition.Child
-		// 			as={Fragment}
-		// 			enter="ease-out duration-300"
-		// 			enterFrom="opacity-0"
-		// 			enterTo="opacity-100"
-		// 			leave="ease-in duration-200"
-		// 			leaveFrom="opacity-100"
-		// 			leaveTo="opacity-0"
-		// 		>
-		// 			<div className="fixed inset-0 bg-black bg-opacity-25" />
-		// 		</Transition.Child>
-
-		// 		<div className="fixed inset-0 overflow-y-auto">
-		// 			<div className="flex min-h-full items-center justify-center p-4 text-center">
-		// 				<Transition.Child
-		// 					as={Fragment}
-		// 					enter="ease-out duration-300"
-		// 					enterFrom="opacity-0 scale-95"
-		// 					enterTo="opacity-100 scale-100"
-		// 					leave="ease-in duration-200"
-		// 					leaveFrom="opacity-100 scale-100"
-		// 					leaveTo="opacity-0 scale-95"
-		// 				>
-		// 					<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-		// 						<Dialog.Title
-		// 							as="h3"
-		// 							className="text-lg font-medium leading-6 text-gray-900"
-		// 						>
-		// 							Payment successful
-		// 						</Dialog.Title>
-		// 						<div className="mt-2">
-		// 							<p className="text-sm text-gray-500">
-		// 								Your payment has been successfully submitted. We’ve sent you
-		// 								an email with all of the details of your order.
-		// 							</p>
-		// 						</div>
-
-		// 						<div className="mt-4">
-		// 							<button
-		// 								type="button"
-		// 								className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-		// 								onClick={closeModal}
-		// 							>
-		// 								Got it, thanks!
-		// 							</button>
-		// 						</div>
-		// 					</Dialog.Panel>
-		// 				</Transition.Child>
-		// 			</div>
-		// 		</div>
-		// 	</Dialog>
-		// </Transition>
 		<>
 			<Transition appear show={isOpen} as={Fragment}>
 				<Dialog as="div" className="relative z-10" onClose={closeModal}>
@@ -104,7 +52,7 @@ const CarDetails = ({ isOpen, closeModal, car }) => {
 									<div className="flex-1 flex flex-col gap-3">
 										<div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-xl">
 											<Image
-												src="/hero2.png"
+												src={generateCarImageUrl(car)}
 												fill
 												alt="car model"
 												priority
@@ -115,7 +63,7 @@ const CarDetails = ({ isOpen, closeModal, car }) => {
 										<div className="flex gap-3 ">
 											<div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-xl">
 												<Image
-													src="/hero2.png"
+													src={generateCarImageUrl(car, "29")}
 													fill
 													alt="car model"
 													priority
@@ -124,7 +72,7 @@ const CarDetails = ({ isOpen, closeModal, car }) => {
 											</div>
 											<div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-xl">
 												<Image
-													src="/hero2.png"
+													src={generateCarImageUrl(car, "33")}
 													fill
 													alt="car model"
 													priority
@@ -133,7 +81,7 @@ const CarDetails = ({ isOpen, closeModal, car }) => {
 											</div>
 											<div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-xl">
 												<Image
-													src="/hero2.png"
+													src={generateCarImageUrl(car, "13")}
 													fill
 													alt="car model"
 													priority
