b6bebf862bd596ac4e23a06ec0117877c5498643
yashPundhir <yash.pundhir.prof@gmail.com>
2023-08-14 23:52:58 +0530

    export const generateCarImageUrl = (car, angle) => {

--- utils/index.js
+++ utils/index.js
@@ -32,3 +32,21 @@ export const calculateCarRent = (city_mpg, year) => {
 
 	return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car, angle) => {
+	const url = new URL("https://cdn.imagin.studio/getimage");
+
+	const { make, year, model } = car;
+
+	url.searchParams.append(
+		"customer",
+		process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
+	);
+	url.searchParams.append("make", make);
+	url.searchParams.append("modelFamily", model.split(" ")[0]);
+	url.searchParams.append("zoomType", "fullscreen");
+	url.searchParams.append("modelYear", `${year}`);
+	url.searchParams.append("angle", `${angle}`);
+
+	return `${url}`;
+};
