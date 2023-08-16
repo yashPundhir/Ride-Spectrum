3acf55d57cb3630bc97672701228c733c915f66c
yashPundhir <yash.pundhir.prof@gmail.com>
2023-08-16 23:25:00 +0530

    smooth scroll to car catalogue & transparent navbar with blur effect added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,25 +1,35 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 
 import { CustomButton } from "./";
 
 const Navbar = () => {
+	const handleClick = () => {
+		const element = document.querySelector("#discover");
+		if (element) {
+			element.scrollIntoView({ behavior: "smooth" });
+		}
+	};
+
 	return (
-		<header className="w-full absolute z-10">
+		<header className="w-full z-20 bg-opacity-20 backdrop-blur-md top-0 fixed">
 			<nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-6">
 				<Link href="/" className="flex justify-center items-center">
 					<Image
-						src="/logo.png"
+						src="/logo2.png"
 						alt="logo"
 						width={300}
 						height={50}
-						className="object-contain"
+						className="object-contain sm:w-[300px] xs:w-[200px] w-[180px]"
 					/>
 				</Link>
 				<CustomButton
-					title="Sign In"
+					title="Scroll to Car Catalogue"
 					btnType="button"
-					containerStyles="text-primary-blue rounded-full bg-white hover:bg-primary-blue hover:text-white min-w-[130px] border-2 border-blue-500 font-semibold text-lg"
+					handleClick={handleClick}
+					containerStyles="text-primary-blue rounded-full bg-white  min-w-[130px] border-2 border-blue-500 font-bold xs:text-sm text-xs sm:text-lg sm:w-auto xs:w-[180px] w-[150px] "
 				/>
 			</nav>
 		</header>
